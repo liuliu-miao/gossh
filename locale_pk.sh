953658d1aac0c06e4436ed277d88513a1ae1e6bc
liubs <liubs@dianzhong.com>
2021-12-14 01:27:00 +0800

    update

--- locale_pk.sh
+++ locale_pk.sh
@@ -1,2 +1,6 @@
+# echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | sudo tee /etc/apt/sources.list.d/goreleaser.list
+# sudo apt update
+# sudo apt install goreleaser
 rm -rf dist
 goreleaser release --snapshot --skip-publish --rm-dist
+
